9d6ed08c2eaf5325f2aa56ae50b627aeefc21b96
tracyvincent <tracykvincent@gmail.com>
2016-07-20 10:43:22 -0500

    answers updated from walkthrough

--- joins-solution.sql
+++ joins-solution.sql
@@ -5,14 +5,17 @@ SELECT * FROM customers FULL OUTER JOIN addresses ON customers.id = addresses.cu
 SELECT * FROM orders JOIN line_items ON orders.id = line_items.order_id;
 
 -- 3. Which warehouses have cheetos?
-SELECT * FROM warehouse JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id WHERE warehouse_product.product_id = 5;
+SELECT warehouse.warehouse FROM products JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id WHERE products.description LIKE 'cheeto%';
 
 -- 4. Which warehouses have diet pepsi?
-SELECT * FROM warehouse JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id WHERE warehouse_product.product_id = 6;
+SELECT warehouse.warehouse FROM products JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id WHERE products.description LIKE 'diet pepsi';
 
 -- 5. Get the number of orders for each customer.
 --NOTE: It is OK if those without orders are not included in results.
-SELECT * FROM customers JOIN addresses ON customers.id = addresses.customer_id JOIN orders ON addresses.id = orders.address_id;
+SELECT customers.*, COUNT(orders.id) FROM customers JOIN addresses ON customers.id = addresses.customer_id
+JOIN orders ON addresses.id = orders.address_id GROUP BY customers.id;
 
 -- 6. How many customers do we have?
 SELECT COUNT(id) FROM customers;
@@ -21,4 +24,5 @@ SELECT COUNT(id) FROM customers;
 SELECT COUNT(id) FROM products;
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
-SELECT SUM(on_hand) FROM warehouse_product JOIN products ON warehouse_product.product_id = products.id WHERE products.id = 6;
+SELECT SUM(on_hand) FROM warehouse_product JOIN products ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id WHERE products.description LIKE 'diet pepsi';
